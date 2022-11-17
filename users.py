49a431f9a5c2c7d4ca7717c249f7c0970753cc12
Ada Rautiainen <ada.rautiainen@gmail.com>
2022-11-17 11:25:01 +0200

    updates

--- users.py
+++ users.py
@@ -2,6 +2,7 @@ from db import db
 from flask import session, request, abort
 from werkzeug.security import check_password_hash, generate_password_hash
 
+#user login
 def login(username, password):
     sql = "SELECT id, password, role FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
@@ -34,3 +35,4 @@ def require_role(role):
 
 def user_id():
     return session.get("user_id", 0)
+
